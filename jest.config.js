fe74144af6e960e9281ed8b4a935f4e1b7c3fe6e
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-01 17:07:54 -0500

    Set up testing configuration files

--- jest.config.js
+++ jest.config.js
@@ -8,12 +8,15 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@components/(.*)$': '<rootDir>/components/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+    '^framer-motion': require.resolve('framer-motion')
   },
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
+  modulePaths: ['<rootDir>'],
+  moduleDirectories: ['node_modules', '<rootDir>']
 }
-
-module.exports = createJestConfig(customJestConfig)
