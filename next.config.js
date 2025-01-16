ed26a95182c2616c343f2c3d3636030030ef91ae
Larry Buckalew <larry.buckalew@gmail.com>
2025-01-16 09:31:16 -0500

    Update configuration files

--- next.config.js
+++ next.config.js
@@ -1,9 +1,1 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    serverActions: true,
-    serverComponents: true
-  }
-}
+module.exports = {}
